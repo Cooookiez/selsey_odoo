412ec23ddf3587742025f807eb3542161ed2ce8d
Cookiez <kukizk@gmail.com>
2022-08-10 22:26:44 +0200

    security + model
    but not working? (at last can't check)

--- odoo/addons/sale_archive/models/order.py
+++ odoo/addons/sale_archive/models/order.py
@@ -5,7 +5,10 @@ class SaleOrderArchive(models.Model):
     _name = 'sale.order.archive'
     _description = "Sale Order Archives"
 
-    # name (text), Order create Date, Count of order lines (int)
     display_name = fields.Char(string='Display Name')
     create_date = fields.Datetime(string='Order create date')
+    client_order_ref = fields.Reference([('res.partner', 'testy')], string='Customer Reference')
+    member_ids = fields.Reference([('res.user', 'testy2')], string='SalePerson')
+    currency_id = fields.Reference([('res.currency', 'testy3')], string='Order Currency')
+    amount_total = fields.Monetary('Order Total Amount', currency_field="currency_id", string="Order Total Amount", store=True)
     order_lines = fields.Integer(string='Count of Order Lines')
